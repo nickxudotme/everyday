70e6de3750ce24297b6312a9b298bded6dc7267e
nickxudotme <nx@nickxu.me>
2026-05-07 03:10:52 +0800

    Use date-based note URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://everyday.nickxu.me',
+  trailingSlash: 'never',
   markdown: {
     shikiConfig: {
       themes: {
